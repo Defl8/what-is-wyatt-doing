1bcc37bd8a09597d295a579adf198ff51ab04967
Wyatt James <wyattsamueljames@gmail.com>
2025-12-03 19:37:50 -0700

    feat(requests): add make get request

--- internal/requests/request_handler.go
+++ internal/requests/request_handler.go
@@ -1,13 +1,42 @@
 package requests
 
+import "net/http"
+
+type RequestType int
+
+const (
+	GET RequestType = iota
+	POST
+	PUT
+	DELETE
+)
+
 type RequestHandler struct {
 	Endpoint string
-	Headers  map[string]any
 }
 
-func NewRequestHandler(endpoint string, headers map[string]any) *RequestHandler {
+func NewRequestHandler(endpoint string) *RequestHandler {
 	return &RequestHandler{
 		Endpoint: endpoint,
-		Headers:  headers,
 	}
+}
+
+func (rH RequestHandler) MakeGetRequest(headers map[string]string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", rH.Endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build headers for request
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
