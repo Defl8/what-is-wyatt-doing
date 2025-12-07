f9d8e9b438f1a9b6790bc053884f79d7051bf651
Wyatt James <wyattsamueljames@gmail.com>
2025-12-07 14:22:43 -0700

    feat: add github like styling and custom date formatting

--- internal/github/event.go
+++ internal/github/event.go
@@ -1,9 +1,12 @@
 package github
 
 import (
+	"fmt"
 	"strings"
 	"time"
 )
+
+const GHBaseURL string = "https://github.com/"
 
 type EventType string
 
@@ -17,6 +20,7 @@ type Event struct {
 	Type      EventType  `json:"type"`
 	Actor     Actor      `json:"actor"`
 	Repo      Repository `json:"repo"`
+	Payload   Payload    `json:"payload"`
 	Public    bool       `json:"public"`
 	CreatedAt time.Time  `json:"created_at"`
 }
@@ -26,16 +30,66 @@ type DisplayEvent struct {
 	Type      string
 	RepoName  string
 	RepoURL   string
+	EventURL  string
 	Timestamp string
 }
 
 func (e Event) Display() DisplayEvent {
 	eventType := strings.Split(string(e.Type), "Event")[0]
+
+	eventURL := ""
+	switch e.Type {
+	case PushEvent:
+		eventURL = GHBaseURL + e.Repo.Name + "/commit/" + e.Payload.Head
+	case CreateEvent:
+		if e.Payload.RefType == "branch" {
+			eventURL = GHBaseURL + e.Repo.Name + "/tree/" + e.Payload.Ref
+		} else {
+			eventURL = GHBaseURL + e.Repo.Name + "/releases/tag/" + e.Payload.Ref
+		}
+	default:
+		eventURL = GHBaseURL + e.Repo.Name
+	}
+
 	return DisplayEvent{
 		ID:        e.ID,
 		Type:      eventType,
 		RepoName:  e.Repo.Name,
-		RepoURL:   "https://github.com/" + e.Repo.Name,
-		Timestamp: e.CreatedAt.Local().Format("2006-01-02 15:04:05"),
+		RepoURL:   GHBaseURL + e.Repo.Name,
+		EventURL:  eventURL,
+		Timestamp: GetRelativeTime(e.CreatedAt),
 	}
+}
+
+func GetRelativeTime(unformattedTime time.Time) string {
+	now := time.Now()
+	diff := now.Sub(unformattedTime)
+	hours := int(diff.Hours())
+
+	if hours < 1 {
+		return "just now"
+	}
+
+	if hours == 1 {
+		return "one hour ago"
+	}
+
+	if hours < 24 {
+		return fmt.Sprintf("%d hours ago", hours)
+	}
+
+	days := hours / 24
+
+	if days == 1 {
+		return "1 day ago"
+	}
+
+	if days < 7 {
+		return fmt.Sprintf("%d days ago", days)
+	}
+
+	if unformattedTime.Year() == now.Year() {
+		return unformattedTime.Local().Format("Jan 02")
+	}
+	return unformattedTime.Local().Format("Jan 02, 2006")
 }
--- internal/github/payload.go
+++ internal/github/payload.go
@@ -1,6 +1,8 @@
 package github
 
 type Payload struct {
-	Head   string `json:"head"`
-	Before string `json:"before"`
+	Head    string `json:"head"`
+	Before  string `json:"before"`
+	Ref     string `json:"ref"`
+	RefType string `json:"ref_type"`
 }
